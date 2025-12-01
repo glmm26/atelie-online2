7f857a1f13e7362b66e8b9160dd3ca543584fc67
Glmmm <guiffracasso20@gmail.com>
2025-11-30 21:29:21 -0300

    Delete atelie_online directory

--- atelie_online/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# Importar todos os modelos para que o SQLAlchemy os reconheça
-from .material_model import Material
--- atelie_online/static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("JS do Ateliê Online carregado!");
